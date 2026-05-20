3b1237db713ade302d4baddcb707125ac435bace
emilio galdeano <emiliogaldeano@MacBook-Air-de-Jose.local>
2026-05-20 14:32:17 -0300

    Nuevo diseño cálido con WhatsApp

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import './globals.css'
+
+export default function Home() {
+  return (
+    <iframe 
+      src="/index.html" 
+      style={{width:'100vw', height:'100vh', border:'none'}}
+    />
+  )
+}
